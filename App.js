d5727842ac972a21d2df0095371b7fc1d41a2a03
ammar10 <ammar.tariq@hotmail.com>
2018-04-12 21:11:38 +0500

    login added

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 
 import { StackNavigator } from 'react-navigation';
-import  Login from './src/components/login/login'
-import  Home from './src/components/home/home'
+import Login from './src/components/login/login';
+import Home from './src/components/home/home';
 export default class App extends React.Component {
-
-  render() {
-    return (
-      <AppNavigator />
-    );
-  }
+	render() {
+		return <AppNavigator />;
+	}
 }
 const AppNavigator = StackNavigator({
-  Login: {screen:Login},
-  Home: {screen:Home},
-  
+	Login: { screen: Login },
+	Home: { screen: Home },
 });
-
